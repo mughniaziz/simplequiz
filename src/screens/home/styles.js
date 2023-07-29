07cf091ef4fe3a9a406bd238f46c5dcc484bd56e
Hafif I <hafifimammuddyn@gmail.com>
2023-07-29 08:11:31 +0700

    💄 Bugfix readability of question and answers

--- src/screens/home/styles.js
+++ src/screens/home/styles.js
@@ -8,6 +8,7 @@ const useStyles = colors =>
     },
     questionBox: {
       alignItems: 'center',
+      backgroundColor: colors.background,
       borderWidth: 1,
       borderColor: colors.border,
       borderRadius: 15,
@@ -21,6 +22,7 @@ const useStyles = colors =>
       paddingHorizontal: 20,
     },
     btnAns: {
+      backgroundColor: colors.background,
       borderWidth: 1,
       borderColor: colors.border,
       alignItems: 'center',
@@ -29,9 +31,13 @@ const useStyles = colors =>
       borderRadius: 5,
     },
     correct: {
+      backgroundColor: colors.background,
+      borderRadius: 5,
       fontSize: 24,
       fontWeight: '700',
       color: colors.text,
+      paddingHorizontal: 15,
+      marginBottom: 15,
     },
   });
 
