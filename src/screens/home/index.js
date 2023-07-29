db60b9bf39689136ff0e529541a59c15dbf809ed
Hafif I <hafifimammuddyn@gmail.com>
2023-07-29 08:32:13 +0700

    🐛 Bugfix: answer status pop up validation, bugfix question update only when user answer is correct

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,14 +1,12 @@
 import React, {useState} from 'react';
 import {View, Text, TouchableOpacity, ImageBackground} from 'react-native';
-import {useDispatch, useSelector} from 'react-redux';
+import {useSelector} from 'react-redux';
 import {useTheme} from '@react-navigation/native';
 import useStyles from './styles';
 import BGHOME from '../../assets/images/bgHome.jpg';
 
 function Home({navigation}) {
-  const dispatch = useDispatch();
   const {filterQuestion} = useSelector(state => state.core);
-  console.log({filterQuestion});
   const {colors} = useTheme();
   const styles = useStyles(colors);
   const [qIndex, setQIndex] = useState(0);
@@ -18,51 +16,49 @@ function Home({navigation}) {
 
   const handleCorrect = answer => {
     if (answer === question.answer) {
-      if (question.isCorrect) {
-        setShowSuccess(true);
-      } else {
-        setShowError(true);
-      }
+      setShowSuccess(true);
+    } else {
+      setShowError(true);
     }
 
     const nextQ = qIndex + 1;
-    console.log({quest: filterQuestion[qIndex]});
+
     const newArrQuest = filterQuestion.map(itm =>
       itm.answer === answer
         ? {...itm, isCorrect: true}
         : {...itm, isCorrect: false},
     );
-    setQuestion(newArrQuest[nextQ]);
-    console.log({newArrQuest});
-    if (nextQ < filterQuestion.length) {
-      if (question.isCorrect) {
+
+    if (answer === question.answer) {
+      if (nextQ < filterQuestion.length) {
+        setQuestion(newArrQuest[nextQ]);
         setQIndex(nextQ);
       } else {
-        setQIndex(qIndex);
+        navigation.navigate('level');
       }
-    } else {
-      navigation.navigate('level');
     }
+
     setTimeout(() => {
       setShowSuccess(false);
       setShowError(false);
-    }, 300);
+    }, 3000);
   };
-
-  console.log({question});
 
   return (
     <View style={styles.container}>
       <ImageBackground source={BGHOME} resizeMode="cover" style={styles.imgBg}>
-        {showSuccess && (
+        {showSuccess ? (
           <Text style={styles.correct}>YA JAWABANMU BENAR!!</Text>
-        )}
-        {showError && <Text style={styles.correct}>YA JAWABANMU SALAH!!</Text>}
+        ) : showError ? (
+          <Text style={styles.correct}>YA JAWABANMU SALAH!!</Text>
+        ) : null}
+
         <View style={styles.questionBox}>
           <Text>{question?.question ?? ''}</Text>
         </View>
+
         <View>
-          {question.choices.map(item => (
+          {question?.choices?.map(item => (
             <TouchableOpacity
               key={item.onPress}
               style={styles.btnAns}
